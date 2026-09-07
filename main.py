5868ff128ba5d4ceeaabd6aee6ce27b6e3c2933e
Chad Z <gelinas.cw@gmail.com>
2026-09-07 11:43:12 -0400

    Allow Vercel frontend in CORS

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://localhost:5173",
+    allow_origins=[
+    "http://localhost:5173",
+    "https://ev-energy-monitor.vercel.app",
+],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
